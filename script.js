5d28ce75c4cd83aad033bae852011032e6199f64
chhotu marandi <cmarandi29@gmail.com>
2022-05-08 17:13:47 +0530

    add buttons feature

---
+++ script.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+const value = document.getElementById("value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")) {
+            count--;
+        }
+        else if(styles.contains("increase")) {
+            count++;
+        }
+        if(styles.contains("reset")) {
+            count = 0;
+        }
+        value.textContent = count;
+    })
+})
